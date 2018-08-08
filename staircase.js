20c0fa6928bd19f0eeb98b6caef7ada1a3c07eed
Kevin Whiteside <kevin.whiteside@esosolutions.com>
2018-08-08 14:27:37 -0500

    staricase problem

---
+++ staircase.js
@@ -0,0 +1,54 @@
+
+/**
+ * Staircase with N steps, and you can climb 1 or 2 steps at a time.
+ * Given N, write a function that returns the number of unique ways you can climb the staircase.
+ * 
+ * 
+ */
+
+
+const stepsAllowedForThisProblem = [1, 2, 3, 5];
+
+
+function HowManyWays(steps, stepsAllowedArray, solved) {
+    if (steps == 0) { return 1 }
+
+    solved = solved || [[]];
+    let total = 0;
+
+    for (let stepsAllowed of stepsAllowedArray) {
+        if (solved[steps] && solved[steps][stepsAllowed]) {
+            total = solved[steps][stepsAllowed];
+            break;
+        }
+        if (steps >= stepsAllowed) {
+            total += HowManyWays(steps - stepsAllowed, stepsAllowedArray, solved);
+        }
+        if (!solved[steps]) {
+            solved[steps] = [];
+        }
+        solved[steps][stepsAllowed] = total;
+    };
+    return total;
+}
+
+
+
+
+const threeAnswer = HowManyWays(3, stepsAllowedForThisProblem);
+console.log("threeAnswer: " + threeAnswer);
+
+const fourAnswer = HowManyWays(4, stepsAllowedForThisProblem);
+console.log("fourAnswer: " + fourAnswer);
+
+const fiveAnswer = HowManyWays(5, stepsAllowedForThisProblem);
+console.log("fiveAnswer: " + fiveAnswer);
+
+const twentythreeAnswer = HowManyWays(23, stepsAllowedForThisProblem);
+console.log("twentythreeAnswer: " + twentythreeAnswer);
+
+const fiftySixAnswer = HowManyWays(56, stepsAllowedForThisProblem);
+console.log("fiftySixAnswer: " + fiftySixAnswer);
+
+const ninetySevenAnswer = HowManyWays(97, stepsAllowedForThisProblem);
+console.log("ninetySevenAnswer: " + ninetySevenAnswer);
