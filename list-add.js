b57ddb1c6c4a425945cbbb48ba750c844e1d7187
Kevin Whiteside <kevin.whiteside@esosolutions.com>
2018-08-09 13:44:16 -0500

    Daily Coding Challenge 8/9

---
+++ list-add.js
@@ -0,0 +1,22 @@
+/**
+    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+    Bonus: Can you do this in one pass?
+ */
+
+/** 
+ * @param {number} desiredSum
+ * @param {Array<number>} givenArray
+ * @returns
+ */
+function CanMakeSum(k, nArray) {
+    let needed = new Set();
+    for (let n of nArray) {
+        if (needed.has(n)) { return true; }
+        else { needed.add(k - n); } // Store compliment for future searches.
+    }
+    return false;
+}
+
+
+console.log(CanMakeSum(15, [15, 0]));
