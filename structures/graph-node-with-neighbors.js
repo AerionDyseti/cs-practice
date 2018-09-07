13fdb5199567b9a5b565da6c43b9337c76bac0d2
Kevin Whiteside <whiteside.kj@gmail.com>
2018-09-06 21:56:40 -0500

    graphing CtCI

---
+++ structures/graph-node-with-neighbors.js
@@ -0,0 +1,12 @@
+
+/** Class representing a single node and it's adjacent nodes.
+ * @class GraphNode
+ */
+module.exports = class GraphNode {
+    constructor(val) {
+        this.val = val;
+        this.neighbors = [];
+    }
+}
+
+
